815ab307975a21e26f4bb4d31746521e1dcd3260
edward <ecguo@ucdavis.edu>
2016-04-28 09:06:51 +0000

    added bias

--- run.py
+++ run.py
@@ -12,30 +12,29 @@ def sigmoid_p(x):
 
 class NN(object):
     def __init__(self):
-        self.alpha = 0.01
-        self.hiddensize = 4
+        self.alpha = .01
+        self.hiddensize = 8 
         self.inputsize = 3
         # initialize weights and bias to a mean of 0
         self.w0 = 2*np.random.random((self.inputsize,self.hiddensize)) - 1
         self.w1 = 2*np.random.random((self.hiddensize,1)) - 1
-        self.b1 = 2*np.random.random((1,self.hiddensize)) - 1
-        self.b2 = 2*np.random.random((1,self.inputsize)) - 1
+        self.b1 = np.zeros((1, self.hiddensize))
+        self.b2 = np.zeros((1, self.inputsize))
 
 
     def _forward_pass(self, X):
         a = []
-        # a.append(sigmoid(np.dot(X, self.w0) + self.b1))
-        # a.append(sigmoid(np.dot(a[0], self.w1) + self.b2))
-        a.append(sigmoid(np.dot(X, self.w0)))
+        a.append(sigmoid(np.dot(X, self.w0) + self.b1))
         a.append(sigmoid(np.dot(a[0], self.w1)))
+        # a.append(sigmoid(np.dot(X, self.w0)))
+        # a.append(sigmoid(np.dot(a[0], self.w1)))
         return a
 
 
     def _backprop(self, X, y, j):
         # set hidden and output layer
         a = self._forward_pass(X)
-        h = a[0]
-        o = a[1]
+        h,o = a[0], a[1]
         # output layer error equals output-output layer
         o_e = y-o
         if (j % 10000) == 0:
@@ -43,17 +42,14 @@ class NN(object):
         # output error delta is equal to the error * the derivative of the
         # logicistic function(sigmoid) of output layer array
         o_d = o_e*sigmoid_p(o)
-        # o_d = np.sum(o_d)
         # hidden layer error equals matrix multiplication of output delta and
         # transposition of w1
         h_e = np.dot(o_d, self.w1.T)
         # hidden error delta is equal to the error * the derivative of the
         # logicistic function(sigmoid) of hidden layer array
         h_d = h_e*sigmoid_p(h)
-        # h_d = np.sum(h_d)
         # update bias and weights
-        # self.b1 += o_d
-        # self.b2 += h_d
+        self.b1 += self.alpha * np.sum(o_d, axis=0)
         self.w1 += self.alpha * np.dot(h.T, o_d)
         self.w0 += self.alpha * np.dot(X.T, h_d)
 
@@ -64,7 +60,7 @@ class NN(object):
         inputs = [ ([563, 566, 5.2], 0), ([492, 523, 6.54], 0), ([487, 602, 7.08], 0), ([553,589,6.22], 0), ([72, 209, 3.02], 1), ([109, 12, 0.18], 1), ([652, 691, 4.18], 0),([300, 596, 4.66], 0),([211, 28, 0.24], 1),([802, 687, 1.38], 1),([875, 550, 2.42], 1) ]
         x_input = np.array([x[0] for x in inputs])
         y_input = [[x[1]] for x in inputs]
-        for i in range(200000):
+        for i in range(100000):
             self._backprop(x_input, y_input, i)
 
     def predict(self, x):
