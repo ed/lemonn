516485a3447c6cf7b33fe4303b31318d70b126a4
ed <ed@users.noreply.github.com>
2017-03-08 09:41:49 -0800

    Update run.py

--- run.py
+++ run.py
@@ -3,8 +3,8 @@ import utils
 import nn as neural
 
 def main():
-    annotated = 'inputs/0016_17_45_04_1_100to400_goldstd_dbl_bs_cosumtvd.csv'
-    metadata = 'inputs/0016_17_45_04_v3_4_9__breath_meta.csv'
+    annotated = ''
+    metadata = ''
     x, y = utils.preprocess(annotated,metadata)
     x = np.array(x)
     nn = neural.NN(learning_rate=.0001, hidden_size=128, input_size=22, output_size=1)
